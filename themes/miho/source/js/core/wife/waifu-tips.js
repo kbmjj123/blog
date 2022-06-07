2d547b4a011ff224160d3f90fd5e912aee36bf85
郑耿林 <zhenggl@tututech.net>
2022-06-07 10:42:28 +0800

    feat: 调整wife为本地的引用，避免因三方服务跪了导致访问不了
    1、调整wife为本地的引用，避免因三方服务跪了导致访问不了

--- themes/miho/source/js/core/wife/waifu-tips.js
+++ themes/miho/source/js/core/wife/waifu-tips.js
@@ -438,7 +438,8 @@ function loadModel(modelId, modelTexturesId){
     localStorage.setItem('modelId', modelId);
     if (modelTexturesId === undefined) modelTexturesId = 0;
     localStorage.setItem('modelTexturesId', modelTexturesId);
-    loadlive2d('live2d', '//api.fghrsh.net/live2d/get/?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
+    loadlive2d('live2d', 'https://www.91temaichang.com/js/core/wife/model.json', console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
+    // loadlive2d('live2d', '//api.fghrsh.net/live2d/get/?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
 }
 
 function loadRandModel(){
